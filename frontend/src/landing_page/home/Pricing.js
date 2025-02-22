bf7359ea6ecb3e7c52b0e261e7d44a2dc98b7e6c
Ankit <vishwakarmaankit8419@gmail.com>
2025-02-22 23:26:21 +0530

    adding new content

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -6,7 +6,7 @@ function Pricing() {
       <div className='row'>
 
         <div className='col-4'>
-          <h1 className='mb-3'>Unbeatable pricing</h1>
+          <h1 className='mb-3'>Unbeatable pricing editing by adarsh</h1>
           <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
           <a href='' style={{ textDecoration: "none" }}>See pricing <i class="fa-solid fa-arrow-right"></i></a>
         </div>
